a619ef4c1396ff4a73fa2b8143a3a7cdfcc5d0de
mroxso <mail@pascalrost.de>
2023-06-18 02:02:23 +0200

    send gernated answer (WIP)

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ try:
     # Read env variable and add relays
     env_relays = os.getenv('RELAYS') # None
     if env_relays is None:
-        env_relays = "wss://relay.layer.systems"
+        env_relays = "wss://relay.damus.io"
     for relay in env_relays.split(","):
         print("Adding relay: " + relay)
         relay_manager.add_relay(relay)
@@ -46,15 +46,29 @@ try:
             print("Notice: " + notice_msg.content)
         while relay_manager.message_pool.has_events():
             event_msg = relay_manager.message_pool.get_event()
+            recipient_pubkey = event_msg.event.pubkey
             msg_decrypted = EncryptedDirectMessage()
             msg_decrypted.decrypt(private_key_hex=private_key.hex(), encrypted_message=event_msg.event.content, public_key_hex=event_msg.event.pubkey)
-            # print (event_msg.event.pubkey + " send " + msg_decrypted.cleartext_content)
             currentTime = time.time()
             if(currentTime - 60 < event_msg.event.created_at):
                 if(event_msg.event.id in messages_done):
                     continue
+                print (event_msg.event.pubkey + " send " + msg_decrypted.cleartext_content)
+                print ("-> Generating Answer..")
                 response = gptj.generate(msg_decrypted.cleartext_content, False)
-                print(response)
+                print("--> " + response)
+                print("Sending response to " + event_msg.event.pubkey)
+
+                dm = EncryptedDirectMessage()
+                dm.encrypt(private_key.hex(),
+                    recipient_pubkey=recipient_pubkey,
+                    cleartext_content=response,
+                )
+                dm_event = dm.to_event()
+                dm_event.sign(private_key.hex())
+                relay_manager.publish_event(dm_event)
+                print("Response sent to " + event_msg.event.pubkey)
+
                 messages_done.append(event_msg.event.id)
                 continue
         time.sleep(10)
